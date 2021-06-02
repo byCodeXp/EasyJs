9d2a789b0e7ac5be75cfe19174607c8054becf62
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-03 00:01:31 +0300

    parse cycles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 const app = new Easy({
     data: {
-        "text": "Hello World!"
+        text: "Hello World!",
+        arr: [1, 2, 3],
+        obj: [
+            {
+                name: "duck",
+                text: "123",
+            },
+            {
+                name: "john",
+                text: "some text",
+            },
+            {
+                name: "go",
+                text: "doe"
+            }
+        ]
     },
     methods: {
 
--- js/lib.js
+++ js/lib.js
@@ -9,9 +9,39 @@ class Easy {
 
             const app = $("#app");
 
-            let dom = app.innerHTML;
+            // find and replace cycles
+
+            let allFor = app.querySelectorAll('for');
+            allFor.forEach(oneFor => {
+                let forIn = oneFor.getAttribute('in');
+                let child = oneFor.innerHTML;
+                let text = "";
+
+                if (this.root.data.hasOwnProperty(forIn)) {
+                    this.root.data[forIn].forEach(e => {
+                        let el = child;
+                        if (typeof(e) === "object")
+                        {
+                            el = el.replace("@this", e);
+                            for(let p in e) {
+                                el = el.replace("@:" + p, e[p]);
+                            }
+                        }
+                        else
+                        {
+                            el = el.replace("@this", e);
+                        }
+                        text += el;
+                    })
+                }
+
+                oneFor.innerHTML = "";
+                oneFor.outerHTML = text;
+            });
 
             // find and replace variables
+
+            let dom = app.innerHTML;
 
             for(let data in this.root.data) {
                 dom = dom.replace('@:' + data, this.root.data[data]);
