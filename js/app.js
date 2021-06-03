4abbf65fac2256aac8b0bd4e92914bcaff6b7237
Dima <65868322+byCodeXp@users.noreply.github.com>
2021-06-03 12:52:24 +0300

    example of implementation of api news

--- js/app.js
+++ js/app.js
@@ -1,23 +1,44 @@
-const app = new Easy({
-    data: {
-        text: "Hello World!",
-        arr: [1, 2, 3],
-        obj: [
-            {
-                name: "duck",
-                text: "123",
-            },
-            {
-                name: "john",
-                text: "some text",
-            },
-            {
-                name: "go",
-                text: "doe"
+class Post {
+    constructor(title, description, urlToImage, publishedAt, author, url, category) {
+        this.title = title;
+        this.description = description;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.author = author;
+        this.url = url;
+        this.category;
+    }
+}
+
+const api = `https://newsapi.org/v2/top-headlines?country=us&apiKey=e6ef2cde327f46e3820d0344025b79fc&category=`;
+// const api = `https://newsapi.org/v2/top-headlines?country=us&apiKey=8b869ec3eec745c99e0442c5abf60ccf&category=`;
+let categories = ['business','entertainment','general','health','science','sports','technology'];
+let posts = [];
+
+categories.forEach(category => {
+    fetch(api + category).then(response => {
+        response.json().then(e => {
+            let articles = e.articles;
+            for (let item in articles) {
+                posts.push(new Post(articles[item].title, articles[item].description, articles[item].urlToImage, articles[item].publishedAt, articles[item].author, articles[item].url, category));
             }
-        ]
+        });
+    });
+});
+
+console.log(posts);
+
+let app = new Easy({
+    data: {
+        data: "Hle",
+        posts: null
     },
     methods: {
 
     }
 });
+
+setTimeout(() => {
+    app.root.data.posts = posts;
+    app.boot();
+}, 500);
--- js/lib.js
+++ js/lib.js
@@ -3,52 +3,52 @@ $ = (query) => {
 }
 
 class Easy {
-    constructor(data = null) {
-        this.root = data;
-        this.boot = function () {
+    boot() {
+        let app = $("#app");
 
-            const app = $("#app");
+        // find and replace cycles
 
-            // find and replace cycles
+        let allFor = app.querySelectorAll('for');
+        allFor.forEach(oneFor => {
+            let forIn = oneFor.getAttribute('in');
+            let child = oneFor.innerHTML;
+            let text = "";
 
-            let allFor = app.querySelectorAll('for');
-            allFor.forEach(oneFor => {
-                let forIn = oneFor.getAttribute('in');
-                let child = oneFor.innerHTML;
-                let text = "";
-
-                if (this.root.data.hasOwnProperty(forIn)) {
-                    this.root.data[forIn].forEach(e => {
-                        let el = child;
-                        if (typeof(e) === "object")
-                        {
-                            el = el.replace("@this", e);
-                            for(let p in e) {
-                                el = el.replace("@:" + p, e[p]);
-                            }
+            if (this.root.data.hasOwnProperty(forIn)) {
+                this.root.data[forIn].forEach(e => {
+                    let el = child;
+                    if (typeof(e) === "object")
+                    {
+                        el = el.replace("@this", e);
+                        for(let p in e) {
+                            el = el.replace("@:" + p, e[p]);
                         }
-                        else
-                        {
-                            el = el.replace("@this", e);
-                        }
-                        text += el;
-                    })
-                }
+                    }
+                    else
+                    {
+                        el = el.replace("@this", e);
+                    }
+                    text += el;
+                })
 
                 oneFor.innerHTML = "";
                 oneFor.outerHTML = text;
-            });
-
-            // find and replace variables
-
-            let dom = app.innerHTML;
-
-            for(let data in this.root.data) {
-                dom = dom.replace('@:' + data, this.root.data[data]);
             }
+        });
 
-            app.innerHTML = dom;
+        // find and replace variables
+
+        let dom = app.innerHTML;
+
+        for(let data in this.root.data) {
+            dom = dom.replace('@:' + data, this.root.data[data]);
         }
+
+        app.innerHTML = dom;
+    }
+
+    constructor(data = null) {
+        this.root = data;
         document.addEventListener('DOMContentLoaded', () => {
             this.boot();
         });
